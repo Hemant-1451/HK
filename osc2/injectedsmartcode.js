1ad1efa0db59b544e2d93a0c9c2f870b74352140
Hemant Kumar <69074914+Hemant-1451@users.noreply.github.com>
2024-08-21 13:53:18 +0530

    Update injectedsmartcode.js

--- osc2/injectedsmartcode.js
+++ osc2/injectedsmartcode.js
@@ -1,9 +1,5 @@
-// injectScript.js
-
-(function() {
-    var script = document.createElement('script');
-    script.src = "https://dev.visualwebsiteoptimizer.com/lib/30000112.js";
-    script.id = "vwoCode";
-    script.referrerPolicy = "no-referrer-when-downgrade";
-    document.head.appendChild(script);
-})();
+const script = document.createElement('script');
+script.src = "https://dev.visualwebsiteoptimizer.com/lib/918943.js";
+script.id = "vwoCode";
+script.referrerPolicy = "no-referrer-when-downgrade";
+document.head.appendChild(script);
